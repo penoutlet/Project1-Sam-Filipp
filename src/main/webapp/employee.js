9c525380ea8c8c744063204f85f5f75f2916e526
Sam Jones <sjonesmusician@gmail.com>
2019-08-18 18:44:20 -0400

    autumn mug background image

--- src/main/webapp/employee.js
+++ src/main/webapp/employee.js
@@ -7,8 +7,6 @@ addbtn.addEventListener("click", function(){
 
 let submitBtn = document.getElementById("submit-btn")
 submitBtn.addEventListener("click", function(e){
-  
-  
   e.preventDefault();
   let ticketArea = document.getElementById("ticket-area");
   let ticketObj = {}
@@ -39,12 +37,91 @@ submitBtn.addEventListener("click", function(e){
 </div>`;
   // ticketObj = JSON.stringify(ticketObj)
   // console.log(JSON.stringify(ticketObj))
-
+  console.log(longCard)
   ticketArea.insertAdjacentHTML('beforeend', longCard )
 
 
-  
+ 
 })
+let newTicketForm = document.getElementById("ticketForm");
+
+newTicketForm.addEventListener("submit", function(e){
+  e.preventDefault();
+  let ticketArea = document.getElementById("ticket-area");
+  let ticketObj = {}
+  let description = document.getElementById("descriptionInput").value
+  let ticketAuthor;
+  let ticketAmount = document.getElementById("reimbursement-amount").value;
+
+  let radios = document.getElementsByName("exampleRadios")
+  let checked = ""
+  for(i=0;i<radios.length;i++){
+    if(radios[i].checked){
+      checked += radios[i].value
+    }
+  }
+  ticketObj.description = description;
+  ticketObj.reimbursement_type = checked;
+  ticketObj.author = 1;
+  ticketObj.ticketAmount = ticketAmount;
+  console.log(ticketObj)
+  // alert(ticketObj)
+  let longCard = `<div class="card" style="width: 60rem;">
+  <div class="card-body">
+    <h5 class="card-title">${ticketObj.reimbursement_type} $${ticketObj.ticketAmount}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${ticketObj.author}</h6>
+    <p class="card-text">${ticketObj.description}.</p>
+    <a href="#" class="card-link">More Info</a>
+  </div>
+</div>`;
+  // ticketObj = JSON.stringify(ticketObj)
+  // console.log(JSON.stringify(ticketObj))
+  console.log(longCard)
+  ticketArea.insertAdjacentHTML('beforeend', longCard )
+
+
+ 
+});
+
+// function createTicket(e) {
+//   e.preventDefault();
+//   let ticketArea = document.getElementById("ticket-area");
+//   let ticketObj = {}
+//   let description = document.getElementById("descriptionInput").value
+//   let ticketAuthor;
+//   let ticketAmount = document.getElementById("reimbursement-amount").value;
+
+//   let radios = document.getElementsByName("exampleRadios")
+//   let checked = ""
+//   for(i=0;i<radios.length;i++){
+//     if(radios[i].checked){
+//       checked += radios[i].value
+//     }
+//   }
+//   ticketObj.description = description;
+//   ticketObj.reimbursement_type = checked;
+//   ticketObj.author = 1;
+//   ticketObj.ticketAmount = ticketAmount;
+//   console.log(ticketObj)
+//   // alert(ticketObj)
+//   let longCard = `<div class="card" style="width: 60rem;">
+//   <div class="card-body">
+//     <h5 class="card-title">${ticketObj.reimbursement_type} $${ticketObj.ticketAmount}</h5>
+//     <h6 class="card-subtitle mb-2 text-muted">${ticketObj.author}</h6>
+//     <p class="card-text">${ticketObj.description}.</p>
+//     <a href="#" class="card-link">More Info</a>
+//   </div>
+// </div>`;
+//   // ticketObj = JSON.stringify(ticketObj)
+//   // console.log(JSON.stringify(ticketObj))
+//   console.log(longCard)
+//   ticketArea.insertAdjacentHTML('beforeend', longCard )
+
+
+ 
+  
+  
+// };
 
 // attempting to modularize the event listeners
 
